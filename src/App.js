f0c3712e141431dc6ce3eec3b7e226bf93718309
josefrnandezz <i62fealj@uco.es>
2019-04-29 20:30:38 +0200

    Added random countries and fixed BeerItem

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: null
+      data: null,
+      countries: null,
     };
   }
 
@@ -18,22 +19,36 @@ class App extends Component {
         const Data = response.data;
         this.setState({ data: Data });
       })
+    
+    Axios
+    .get("https://restcountries.eu/rest/v2/all")
+    .then(response => {
+      const Countries = response.data;
+      this.setState({ countries: Countries });
+    })
+  }
+
+  getRandom() {
+    return Math.floor((Math.random() * 249) + 1);
   }
 
   render() {
     return (
-        <List horizontal>
-          {this.state.data && this.state.data.map(element => (
-            <List.Item>
-              <BeerItem 
-                name={element.name} 
-                rating={element.abv}
-                image={element.image_url}
-                onClick={(() => console.log(element.description))}
-              />
-            </List.Item>
-          ))}
-        </List>
+      <List horizontal>
+        {this.state.countries && this.state.data && this.state.data.map(element => (
+          <List.Item>
+            <List.Content>
+                <BeerItem 
+                  name={element.name} 
+                  rating={element.abv}
+                  image={element.image_url}
+                  country={this.state.countries[this.getRandom()].name}
+                  onClick={(() => console.log(element.description))}
+                />
+            </List.Content>
+          </List.Item>
+        ))}
+      </List>
     );
   }
 }
--- src/components/BeerItem/index.js
+++ src/components/BeerItem/index.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { Card, Image } from "semantic-ui-react";
+import { Card, Icon } from "semantic-ui-react";
+import styled from "styled-components";
 
-const BeerItem = ({ name, rating, onClick, image }) => (
+const Image = styled.img`
+  width: 10%;
+  height: 10%;
+  object-fit: cover;
+  margin-bottom: 10px;
+  margin: auto;
+`;
+
+const BeerItem = ({ name, rating, onClick, image, country }) => (
   <Card onClick={onClick}>
-    <Image src={image} height="100" width="30" />
+    <Image src={image} />
     <Card.Content>
       <Card.Header>{name}</Card.Header>
       <Card.Meta>{rating}</Card.Meta>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='world' />
+        {country}
+      </a>
     </Card.Content>
   </Card>
 );
