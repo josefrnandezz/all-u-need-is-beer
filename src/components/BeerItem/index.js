2384637cb8161e4157c75d23d2bbe39dacff21f4
josefrnandezz <i62fealj@uco.es>
2019-04-29 13:19:13 +0200

    Updated App.js and BeerItem with semantic ui components

--- src/components/BeerItem/index.js
+++ src/components/BeerItem/index.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Card, Image } from "semantic-ui-react";
 
-const BeerItem = ({ name, rating, onClick }) => (
-  <div onClick={onClick}>
-    <h1>{name}</h1>
-    <p>Rating: {rating}</p>
-  </div>
+const BeerItem = ({ name, rating, onClick, image }) => (
+  <Card onClick={onClick}>
+    <Image src={image} height="100" width="30" />
+    <Card.Content>
+      <Card.Header>{name}</Card.Header>
+      <Card.Meta>{rating}</Card.Meta>
+    </Card.Content>
+  </Card>
 );
 
 export default BeerItem;
